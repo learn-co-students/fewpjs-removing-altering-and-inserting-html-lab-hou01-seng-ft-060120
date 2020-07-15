f0449d8686f6a9b463461bfeef2131476a8c3f3d
Julio <julio.chazari@gmail.com>
2020-07-15 02:07:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+//const main = document.querySelector("main#main");
+//main.remove();
+document.querySelector("main#main").remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Julio is the champion";
+document.body.appendChild(newHeader);
