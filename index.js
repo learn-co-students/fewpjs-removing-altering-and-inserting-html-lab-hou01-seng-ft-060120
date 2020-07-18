df7ad9336f48c97d9b8b155e679e7d5c380408d2
mcgillshakore <mcgills2093@gmail.com>
2020-07-18 11:42:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code here!
+let element = document.createElement('div');
+    document.body.appendChild(element);
+
+let ul = document.createElement('ul');
+    for (let i = 0; i < 3; i++){
+        let li = document.createElement('li')
+        li.innerHTML = (i + 1).toString()
+        ul.appendChild(li)
+    };
+element.appendChild(ul);
+
+element.style.backgroundColor = '#27647B';
+element.style.textAlign = 'center';
+ul.style.textAlign = 'left';
+
+// element.className = "dog";
+// element.className = "pet-listing dog"
+
+element.classList.remove("this-is-fine");
+element.classList.add("the-room-is-on-fire")
+
+ul.removeChild(ul.querySelector('li:nth-child(2)'))
+ul.remove()
+
+let main = document.querySelector('main#main');
+main.remove()
+
+let yourName = 'Trey'
+
+let newHeader = document.createElement('h1');
+    newHeader.setAttribute("id", "victory")
+    newHeader.innerHTML =( `${yourName} is the champion`)
+    document.body.appendChild(newHeader);
